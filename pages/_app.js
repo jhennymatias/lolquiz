49f10bf60c0daa36554c2be9aebed0bb04834c35
jhennymatias <jhennifercristinematias@gmail.com>
2021-01-26 16:05:24 -0300

    Aula 1 da imersão alura

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,20 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
